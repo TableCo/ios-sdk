8013ea5fcf7c9753ea55bfa3e92473c112c0d8b3
Roman Maiboroda <roman.maiboroda@chisw.com>
2020-09-01 15:37:41 +0300

    Push configuration

--- SDK/Table SDK/SDK/Entities/TableData/TableData.swift
+++ SDK/Table SDK/SDK/Entities/TableData/TableData.swift
@@ -12,14 +12,17 @@ class TableData {
     // Members which don't need to reset
     internal var workspaceUrl = ""
     internal var apiKey = ""
-    internal var token: String?
+    internal var token: String? {
+        didSet {
+            updateNotificationTokenCompletion?(notificationToken)
+        }
+    }
     internal var experienceShortCode: String?
-    internal var fcmNotificationChannel: String?
-    internal var jpushNotificationChannel: String?
+    internal var notificationToken: String?
+    internal var updateNotificationTokenCompletion: ((_ token: String?) -> Void)?
     internal var themeColor = UIColor.init(hexString: "#307AEB")
     internal var userAttributes = UserAttributes()
 
     // Members to reset on logout
     internal var userID: String?
-
 }
--- Sample project/TableSample/ViewController.swift
+++ Sample project/TableSample/ViewController.swift
@@ -16,25 +16,21 @@ class ViewController: UIViewController {
         
         Table.initialize(workspaceUrl: "https://develop4.dev.table.co", apiKey: "978fQmN5ReV3vPKclQgHEg", onSuccessInitializeCompletion: {
             
-        }) { (errorCode, message) in
-            
+        }) { [weak self] (errorCode, errorMessage) in
+            self?.showError(errorCode: errorCode, errorMessage: errorMessage)
         }
         
     }
 
     @IBAction func registerUser(_ sender: Any) {
         var userAttrib = UserAttributes()
-        userAttrib.firstName = "Test"
+        userAttrib.firstName = "Bruice"
         userAttrib.lastName = "User"
-        userAttrib.email = "ambrazhnik@gmail.com"
-        Table.registerUser(withUserId: "1212313214", userAttributes: userAttrib, onSuccessLoginCompletion: { [userAttrib, weak self] in
+        userAttrib.email = "bruice@gmail.com"
+        Table.registerUser(withUserId: "1212313214Bruice", userAttributes: userAttrib, onSuccessLoginCompletion: { [userAttrib, weak self] in
             self?.showAlert(nil, message: "You are logined as \(userAttrib.firstName ?? "") \(userAttrib.lastName ?? "")")
         }) { [weak self](errorCode, errorMessage) in
-            guard let codeInt = errorCode else {
-                self?.showAlert("Error", message: errorMessage ?? "")
-                return
-            }
-            self?.showAlert("Error", message: "Code: \(String(codeInt))  \(errorMessage ?? "")")
+            self?.showError(errorCode: errorCode, errorMessage: errorMessage)
         }
     }
     
@@ -42,22 +38,26 @@ class ViewController: UIViewController {
         Table.registerUnidentifiedUser(onSuccessLoginCompletion: {[weak self] in
             self?.showAlert(nil, message: "You are logined as Anonimous user")
         }) { [weak self] (errorCode, errorMessage) in
-             guard let codeInt = errorCode else {
-                 self?.showAlert("Error", message: errorMessage ?? "")
-                 return
-             }
-             self?.showAlert("Error", message: "Code: \(String(codeInt))  \(errorMessage ?? "")")
+            self?.showError(errorCode: errorCode, errorMessage: errorMessage)
         }
     }
     
     @IBAction func showConversationList(_ sender: Any) {
-        Table.showConversationList(parentViewController: self) { (errorCode, errorMessage) in
-            
+        Table.showConversationList(parentViewController: self) { [weak self] (errorCode, errorMessage) in
+            self?.showError(errorCode: errorCode, errorMessage: errorMessage)
         }
     }
     
     @IBAction func Logout(_ sender: Any) {
         Table.logout()
+    }
+    
+    func showError(errorCode: Int?, errorMessage: String?) {
+        guard let codeInt = errorCode else {
+            showAlert("Error", message: errorMessage ?? "")
+            return
+        }
+        showAlert("Error", message: "Code: \(String(codeInt))  \(errorMessage ?? "")")
     }
 }
 
